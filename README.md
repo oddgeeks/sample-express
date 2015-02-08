79964ab5ccde4760e4df848bd9adc6cbee924eac
Mathrew Tervo <mathrewtervo@gmail.com>
2015-02-08 16:09:21 +0100

    Sample nodejs application

---
+++ README.md
@@ -0,0 +1,8 @@
+Sample Node.JS application
+==========================
+
+This application defines a simple NodeJS application.
+
+Live website: https://sample-node-express.scalingo.io
+
+Documentation: http://doc.scalingo.com/languages/nodejs
---
+++ app.js
@@ -0,0 +1,22 @@
+/**
+ * @author Léo Unbekandt
+ */
+
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index', {});
+})
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('App listening at http://%s:%s', host, port)
+})
+
+
